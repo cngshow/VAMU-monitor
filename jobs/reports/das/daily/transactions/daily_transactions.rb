eb88f56cb396346b5a2745bc6c1cf3c1008e177d
cngshow <cngshow1@gmail.com>
2015-10-21 08:56:46 -0400

    reformatting daily reports

--- jobs/reports/das/daily/transactions/daily_transactions.rb
+++ jobs/reports/das/daily/transactions/daily_transactions.rb
@@ -6,14 +6,16 @@ include JobUtility
 @rpt_date = format_date(ARGV[0], RPT_DATE)
 @plink = ARGV[1]
 
-def putty_call
-  connect = 'dasuser@bhiepapp4.r04.med.va.gov -pw dasprod@123!'
+def putty_call(server = 4)
+  connect = "dasuser@bhiepapp#{server}.r04.med.va.gov -pw dasprod@123!"
   %{#{@plink} #{connect} \"cat ./reports/output/#{@rpt_date}/summary.#{@rpt_date}\"}
 end
 
-str = backtick(putty_call)
+papp4 = backtick(putty_call)
+papp5 = backtick(putty_call(5))
 
 puts "EMAIL_RESULT_BELOW:\n"
 puts "SUBJECT: DAS Daily Transactions Report for #{@rpt_date}"
-puts "<pre>#{str}</pre>"
+puts "<pre>#{papp4}</pre>"
+puts "<pre>#{papp5}</pre>"
 puts 'EMAIL_RESULT_ABOVE:'
